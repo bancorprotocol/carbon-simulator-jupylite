24f83f673bd864c9f760254b4ae380bf8a26e10c
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-02-02 20:38:39 +0000

    version 1.2
    Update version.py

--- content/demo7-4/version.py
+++ content/demo7-4/version.py
@@ -1,11 +1,12 @@
 """
 Demo7-4 -- VERSION NUMBER
 """
-__VERSION__ = "1.1"
-__DATE__ = "01/02/2023"
+__VERSION__ = "1.2"
+__DATE__ = "02/02/2023"
 
 def print_version():
     """
     prints the version of this simulation package
     """
     print(f"Carbon Sim v{__VERSION__} ({__DATE__})")
+    
